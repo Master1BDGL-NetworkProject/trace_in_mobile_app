3f288e7dc6ef60c15f13e6b23a45e7c524a381a0
ofceab <flutternow1963@gmail.com>
2022-04-30 13:42:47 +0000

    Updated server IP

--- lib/core/utils/api_urls.dart
+++ lib/core/utils/api_urls.dart
@@ -7,8 +7,8 @@ abstract class IApiUrls {
 }
 
 class ApiUrlImpl implements IApiUrls {
-  final String _pingUrl = 'http://34.229.230.190:8080/ping';
-  final String _tracerouteUrl = 'http://34.229.230.190:8080/traceroute';
+  final String _pingUrl = 'http://34.207.127.105:8080/ping';
+  final String _tracerouteUrl = 'http://34.207.127.105:8080/traceroute';
 
   @override
   String get pingUrl => _pingUrl;
